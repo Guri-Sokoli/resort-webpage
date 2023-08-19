135b41c37246d622169a51c5bbc103daa4103d6a
Guri Sokoli <gurisokoli1@gmail.com>
2023-08-19 22:05:17 +0200

    fixed it :face-palm:

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -1,46 +1,52 @@
 import InstagramLogo from "../img/Instagram-Logo.svg";
 import FacebookLogo from "../img/Facebook-Logo.svg";
 export const SocialMediaLinks = () => {
-    <div>
-        <button className="px-2">
-            <a href="https://www.instagram.com/">
-                <img
-                    src={InstagramLogo}
-                    alt="instagram logo"
-                    width={25}
-                    height={25}
-                    className=""
-                    fill="#fff"
-                />
-            </a>
-        </button>
-        <button className="px-2">
-            <a href="https://www.facebook.com/">
-                <img
-                    src={FacebookLogo}
-                    alt="facebook logo"
-                    width={25}
-                    height={25}
-                    className=""
-                />
-            </a>
-        </button>
-        <button className="px-2" href="https://www.twitter.com/" fill="#fff">
-            <a href="https://www.twitter.com/">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={25}
-                    height={25}
-                    fill="currentColor"
-                    className="bi bi-twitter"
-                    viewBox="0 0 16 16"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M15.938 3.056a5.572 5.572 0 0 1-1.6.438 2.782 2.782 0 0 0 1.219-1.531 5.564 5.564 0 0 1-1.766.672 2.78 2.78 0 0 0-4.75 2.531 7.9 7.9 0 0 1-5.75-2.922 2.78 2.78 0 0 0 .86 3.703A2.77 2.77 0 0 1 2.5 5.875v.031a2.78 2.78 0 0 0 2.219 2.719 2.775 2.775 0 0 1-.719.094 2.47 2.47 0 0 1-.531-.063 2.78 2.78 0 0 0 2.594 1.922A5.57 5.57 0 0 1 2 12.25a7.85 7.85 0 0 0 4.25 1.25c5.125 0 7.938-4.25 7.938-7.937 0-.125 0-.25-.008-.375A5.65 5.65 0 0 0 16 3.563c-.563.25-1.063.5-1.562.875z"
+    return (
+        <div>
+            <button className="px-2">
+                <a href="https://www.instagram.com/">
+                    <img
+                        src={InstagramLogo}
+                        alt="instagram logo"
+                        width={25}
+                        height={25}
+                        className=""
+                        fill="#fff"
                     />
-                </svg>
-            </a>
-        </button>
-    </div>;
+                </a>
+            </button>
+            <button className="px-2">
+                <a href="https://www.facebook.com/">
+                    <img
+                        src={FacebookLogo}
+                        alt="facebook logo"
+                        width={25}
+                        height={25}
+                        className=""
+                    />
+                </a>
+            </button>
+            <button
+                className="px-2"
+                href="https://www.twitter.com/"
+                fill="#fff"
+            >
+                <a href="https://www.twitter.com/">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={25}
+                        height={25}
+                        fill="currentColor"
+                        className="bi bi-twitter"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M15.938 3.056a5.572 5.572 0 0 1-1.6.438 2.782 2.782 0 0 0 1.219-1.531 5.564 5.564 0 0 1-1.766.672 2.78 2.78 0 0 0-4.75 2.531 7.9 7.9 0 0 1-5.75-2.922 2.78 2.78 0 0 0 .86 3.703A2.77 2.77 0 0 1 2.5 5.875v.031a2.78 2.78 0 0 0 2.219 2.719 2.775 2.775 0 0 1-.719.094 2.47 2.47 0 0 1-.531-.063 2.78 2.78 0 0 0 2.594 1.922A5.57 5.57 0 0 1 2 12.25a7.85 7.85 0 0 0 4.25 1.25c5.125 0 7.938-4.25 7.938-7.937 0-.125 0-.25-.008-.375A5.65 5.65 0 0 0 16 3.563c-.563.25-1.063.5-1.562.875z"
+                        />
+                    </svg>
+                </a>
+            </button>
+        </div>
+    );
 };
