def3f1bc547ba05cf836b39e3d053416a8dd631c
Guri <gurisokoli1@gmail.com>
2023-09-06 22:14:27 +0200

    setup for finishing home :)

--- src/pages/Home/HomeQuickLinks.js
+++ src/pages/Home/HomeQuickLinks.js
@@ -20,7 +20,7 @@ const HomeQuickLinks = () => {
             className="flex flex-col items-center bg-white py-24 px-24"
             style={containerStyle}
         >
-            <div className="flex flex-row bg-white w-full mx-24 text-black">
+            <div className="flex flex-row justify-center items-center bg-white w-full mx-24 text-black">
                 <div className="flex flex-col p-16 w-7/12">
                     <img
                         src={Logo}
@@ -57,14 +57,39 @@ const HomeQuickLinks = () => {
                     </div>
                 </div>
 
-                {/* <div className="flex flex-col justify-center w-full h-max">
-                    <div className="flex flex-col justify-center items-center align-center">
-                        <div>
-                            <p>Test 1</p>
+                <div className="flex flex-col justify-center w-5/12 h-max">
+                    <div className="flex flex-row justify-center items-center align-center">
+                        <div className="flex flex-col justify-start w-full h-full">
+                            <p className="text-2xl">Quick Links</p>
+                            <br />
+                            <ul>
+                                <p>About Us</p>
+                                <p>News</p>
+                                <p>Contact</p>
+                                <p>Book Now</p>
+                            </ul>
                         </div>
-                        <p>Test 2</p>
+                        <div className="flex flex-col justify-start w-full h-full">
+                            <p>Our Cabins</p>
+                            <br />
+                            <ul>
+                                <p>Cabin One</p>
+                                <p>Cabin Two</p>
+                                <p>Cabin Three</p>
+                                <p>Cabin Four</p>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col justify-start w-full h-full">
+                            <br />
+                            <ul>
+                                <p>Cabin Five</p>
+                                <p>Cabin Six</p>
+                                <p>Cabin Seven</p>
+                                <p>Cabin Eight</p>
+                            </ul>
+                        </div>
                     </div>
-                </div> */}
+                </div>
 
                 {/* sjom ka di qysh me centirat qito nalt, might have to take a different approach */}
             </div>
